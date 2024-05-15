d8d574f57b0a6b79c072f1a74ec1017855ceb03c
Jagra <Jagra@JagrathShetty>
2024-05-15 16:15:28 +0530

    Third Class as Automation member

--- DemoAutomationLead/src/test/java/firstPackage/FirstClass.java
+++ DemoAutomationLead/src/test/java/firstPackage/FirstClass.java
@@ -5,5 +5,6 @@ public class FirstClass
 public static void main(String[] args) {
 	System.out.println("First Class as Automation Lead");
 	System.out.println("Second Class as Automation member");
+	System.out.println("Third Class as Automation member");
 }
 }

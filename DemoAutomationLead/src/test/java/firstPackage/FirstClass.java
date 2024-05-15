925249dfaac2d0032d7f640fea349755bafa5d4b
Jagra <Jagra@JagrathShetty>
2024-05-15 16:11:40 +0530

    FirstClass has been modified

--- DemoAutomationLead/src/test/java/firstPackage/FirstClass.java
+++ DemoAutomationLead/src/test/java/firstPackage/FirstClass.java
@@ -4,5 +4,6 @@ public class FirstClass
 {
 public static void main(String[] args) {
 	System.out.println("First Class as Automation Lead");
+	System.out.println("Second Class as Automation member");
 }
 }

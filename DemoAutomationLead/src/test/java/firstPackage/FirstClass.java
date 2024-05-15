24b9f771e3dc75bcc01d934e860365f84db1d34e
Dell <Dell@MRIDULA-KEG9N97>
2024-05-15 16:23:05 +0530

    Fourth added

--- DemoAutomationLead/src/test/java/firstPackage/FirstClass.java
+++ DemoAutomationLead/src/test/java/firstPackage/FirstClass.java
@@ -6,5 +6,6 @@ public static void main(String[] args) {
 	System.out.println("First Class as Automation Lead");
 	System.out.println("Second Class as Automation member");
 	System.out.println("Third Class as Automation member");
+	System.out.println("Fourth Class as Automation Member");
 }
 }
